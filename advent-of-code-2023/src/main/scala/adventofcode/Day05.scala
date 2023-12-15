ca869a3b9647417c7fd5ed21a7367756519bcc01
Matthias Biermans <matthias.biermans@databrewers.io>
2023-12-15 10:27:48 +0100

    Commit of last time I worked on stuff?

--- advent-of-code-2023/src/main/scala/adventofcode/Day05.scala
+++ advent-of-code-2023/src/main/scala/adventofcode/Day05.scala
@@ -3,50 +3,100 @@ package adventofcode
 import scala.util.matching.Regex
 import scala.io.Source
 
+case class MappingRange(
+    lowerBound: Long,
+    upperBound: Long
+) {
+    def containsElement(element: Long): Boolean = {
+        return lowerBound <= element && upperBound >= element
+    }
+}
 
 object Day05 extends App {
-    val seedsRegex: Regex = """seeds: (.+)""".r
-    val rangeRegex: Regex = """(\d+) (\d+) (\d+)""".r
+  val seedsRegex: Regex = """seeds: (.+)""".r
+  val rangeRegex: Regex = """(\d+) (\d+) (\d+)""".r
 
+  def range(input: String): (Long, Long, Long) = {
+    input match {
+      case rangeRegex(dest, source, length) =>
+        (source.toLong, dest.toLong, length.toLong)
+    }
+  }
 
-    def range(input: String): Map[Long, Long] = {
-        input match {
-            case rangeRegex(dest, source, length) => 
-                (0L until length.toInt).map(x => (x + source.toLong, x + dest.toLong)).toMap
+  def getMappings(block: String): List[(Long, Long, Long)] = {
+    (
+      for {
+        line <- block.split("\n").toList.tail
+        r = range(line)
+      } yield r
+    ).foldLeft(List.empty)(_ :+ _)
+  }
+
+  def getSeeds(input: String): List[Long] = {
+    input match {
+      case seedsRegex(s) => s.split(" ").map(_.toLong).toList
+    }
+  }
+
+  def traverse(mapping: List[List[(Long, Long, Long)]])(next: Long): Long = {
+    if (mapping.isEmpty) {
+      next
+    } else {
+      val found = mapping.head
+        .find { case (source, dest, length) =>
+          (source <= next) && (next < source + length)
         }
-    }
-
-    def getMappings(block: String): Map[Long, Long] = {
-        (
-            for {
-                line <- block.split("\n").toList.tail
-                r = range(line)
-                _ = println(r.toList)
-            } yield r
-        ).foldLeft(Map.empty)(_ ++ _)
-    }
-
-    def getSeeds(input: String): List[Long] = {
-        input match {
-            case seedsRegex(s) => s.split(" ").map(_.toLong).toList
+        .map {
+          case (source, dest, length) => {
+            val difference = next - source
+            dest + difference
+          }
         }
+        .getOrElse(next)
+      traverse(mapping.tail)(found)
+    }
+  }
+
+  val fileName = "day_05.txt"
+  val input = Source
+    .fromResource(fileName)
+    .getLines()
+    .mkString("\n")
+    .split("\n\n")
+    .toList
+
+  val seeds = getSeeds(input.head)
+
+  val mappings = input.tail.map(getMappings)
+
+  val result = seeds.map(Day05.traverse(mappings)).min
+
+  val seeds2 = seeds.sliding(2).map {
+    case List(start, length) => {
+      val range = (start until (length + start))
+      range.min -> range.max
+    }
+  }
+
+  def findOverlappingRanges(input: MappingRange, comparison: MappingRange): (Option[MappingRange], List[MappingRange]) = {
+    val ranges = List(
+        input,
+        MappingRange(input.lowerBound, comparison.upperBound),
+        MappingRange(comparison.lowerBound, input.upperBound)
+    )
+
+    val compContainsInput = ranges.find{
+        case MappingRange(low, up) => input.containsElement(low) && input.containsElement(up) && comparison.containsElement(low) && comparison.containsElement(up)
     }
 
-    def traverse(mapping: List[Map[Long, Long]])(next: Long): Long = {
-        if(mapping.isEmpty){
-            next
-        } else {
-            traverse(mapping.tail)(mapping.head.getOrElse(next, next))
-        }
+    find match {
+        case Some(_) =>
+            find -> ranges.filter{
+                        case MappingRange(low, up) => input.containsElement(low) && input.containsElement(up) && !comparison.containsElement(low) && !comparison.containsElement(up)
+                    }
+        case None => 
+            None -> List(input) 
     }
+  }
 
-
-    val fileName = "day_05.txt"
-    val input = Source.fromResource(fileName)
-        .getLines().mkString("\n").split("\n\n").toList
-
-    val seeds = getSeeds(input.head)
-
-    println(seeds)
-    val mappings = input.tail.map(getMappings)
 }
--- advent-of-code-2023/src/test/scala/adventofcode/Day05Test.scala
+++ advent-of-code-2023/src/test/scala/adventofcode/Day05Test.scala
@@ -1,8 +1,34 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import scala.io.Source
+import adventofcode.Day05
+import org.scalatest.matchers.should.Matchers
+import adventofcode.MappingRange
 
-class Day05Test extends AnyFlatSpec {
+class Day05Test extends AnyFlatSpec with Matchers {
 
-    it should "correctly find the mapping for the seed" in {
-        assert(true)
+    trait Day05Fixture {
+        val input = Source.fromResource("test_day_05.txt")
+            .getLines().mkString("\n").split("\n\n").toList
+
+        val seeds = Day05.getSeeds(input.head)
+        val mappings = input.tail.map(Day05.getMappings)
     }
+
+    it should "correctly find the mapping for the seed" in new Day05Fixture {
+
+        seeds.map(Day05.traverse(mappings)) shouldBe List(
+            82L, 43L, 86L, 35L
+        )
+    }
+
+    it should "find overlapping ranges" in new Day05Fixture {
+        Day05.findOverlappingRanges(MappingRange(1L, 10L), MappingRange(2L, 8L)) shouldBe Some(MappingRange(2L, 8L)) -> List.empty
+
+        Day05.findOverlappingRanges(MappingRange(10L, 20L), MappingRange(2L, 8L)) shouldBe None -> List(MappingRange(10L, 20L))
+        
+        // Day05.findOverlappingRanges(MappingRange(1L, 6L), MappingRange(2L, 8L)) shouldBe Some(MappingRange(2L, 6L))
+
+        // Day05.findOverlappingRanges(MappingRange(6L, 20L), MappingRange(2L, 8L)) shouldBe Some(MappingRange(6L, 8L))
+    }
+
 }
